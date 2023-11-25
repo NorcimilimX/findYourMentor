7c972e39e89a9f1a8e03012cbd94de63744321ca
VKomaretskyi <vkomaretskyi@luxoft.com>
2023-11-25 16:30:44 +0200

    add mentors registration form

--- src/pages/mentors/MentorsRegistration.vue
+++ src/pages/mentors/MentorsRegistration.vue
@@ -1,3 +1,19 @@
 <template>
-  Register as a MENTOR!
+  <section>
+    <base-card>
+      <h2>You have some knowledge to share?<br><strong>Register as a mentor now!</strong></h2>
+      <mentor-form></mentor-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import MentorForm from "../../components/mentors/MentorForm.vue";
+import BaseCard from "../../components/ui/BaseCard.vue";
+export default {
+  components: {
+    BaseCard,
+    MentorForm
+  }
+}
+</script>
