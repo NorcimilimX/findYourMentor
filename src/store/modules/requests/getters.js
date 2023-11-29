9a12c5fad6f5578a2d9154acd28586cc34e8645d
VKomaretskyi <vkomaretskyi@luxoft.com>
2023-11-30 01:21:36 +0200

    filter requests for each mentor

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,8 +1,9 @@
 export default {
-    requests(state) {
-        return state.requests
+    requests(state, _, _2, rootGetters) {
+        const mentorId = rootGetters.userId
+        return state.requests.filter(req => req.mentorId === mentorId)
     },
-    hasRequests(state) {
-        return state.requests && state.requests.length > 0
+    hasRequests(_, getters) {
+        return getters.requests && getters.requests.length > 0
     }
 }
