bb2f87f54f07028829445ab4c517c60f78b44fe3
VKomaretskyi <vkomaretskyi@luxoft.com>
2023-12-03 15:54:26 +0200

    not found component

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <base-card>
+      <h2>404 Page not found</h2>
+      <p>This page couldn't be found - check out to all
+        <router-link to="/mentors">mentors.</router-link></p>
+    </base-card>
+  </section>
+</template>
+<script setup>
+import BaseCard from "../components/ui/BaseCard.vue";
+</script>
